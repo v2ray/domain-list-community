1678a2bf7508db58918914ba279f704a53fe9f5b
Loyalsoldier <10487845+Loyalsoldier@users.noreply.github.com>
2020-07-07 13:54:16 +0800

    Change v2ray source from v2ray.com to github.com/v2fly

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 	"strings"
 
 	"github.com/golang/protobuf/proto"
-	"v2ray.com/core/app/router"
+	"github.com/v2fly/v2ray-core/app/router"
 )
 
 var dataPath = flag.String("datapath", "", "Path to the custom data folder")
